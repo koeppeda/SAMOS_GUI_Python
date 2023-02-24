a50d00a0f92928228d2610c242a7fb2bd0af20b6
koeppeda <koeppeda@msu.edu>
2023-02-24 16:15:08 -0500

    reorganized dictionary and added wrapper to track attribute changes
    whenever a parameter is changed, the class automatically updates the main_dict

--- WriteFITSHead.py
+++ WriteFITSHead.py
@@ -13,6 +13,32 @@ import os
 from astropy.io import fits
 
 
+class Wrapper():
+    
+    """
+    Wrapper for main FITSHead class that updates the main dict
+    whenever one of the param attributes is changed.
+    Also keeps track of the original value before the change.
+    """
+    def __init__(self, wrapped):
+        self._wrapped = wrapped
+        
+    def __getattr__(self, attr):
+        return getattr(self._wrapped, attr)
+    
+    def __setattr__(self, attr, val):
+        if attr == '_wrapped':
+            super(Wrapper, self).__setattr__('_wrapped', val)
+        else:
+            setattr(self._wrapped, 'prev_' + attr, getattr(self._wrapped, attr))
+            old_val = getattr(self._wrapped, attr)
+            setattr(self._wrapped, attr, val)
+            self.create_main_params_dict()
+            print("{} changed from {} to {}".format(attr,old_val,val))
+ 
+           
+
+
 class FITSHead():
     """
     Obtain instrument configurations, WCS, etc. and
@@ -25,36 +51,33 @@ class FITSHead():
     Each attribute can the be combined into a main param dict,
     which will get written to header.
     
-
-    
-    
     """
     
     def __init__(self):
         
         self.main_dict = None # main dictionary will be the container for all params
         
-        self.filename = {'FILENAME' : None}
+        self.filename = None
         
-        self.expTime = {'EXPTIME': (None, 'Exposure time (s)')}
-        self.objName = {'OBJECT': (None,'User-defined name of object')} # 'OBJECT' name of object i.e. ABELL S1101
-        self.obsType = {'OBSTYPE': (None, 'Type of observation')} # 'OBSTYPE' type of observation i.e. BIAS, FLAT, OBJ...
-        self.radecSys = {'RADECSYS': ('FK5', 'Default coordinate system')} # prob won't change
-        self.radecEq = {'RADECEQ': (2000, 'Default equinox')} # prob won't change
-        self.ra = {'RA': (None,'RA of object (hr)')}  
-        self.dec = {'DEC': (None,'DEC of object (deg)')} 
+        self.expTime = None
+        self.objName = None # 'OBJECT' name of object i.e. ABELL S1101
+        self.obsType = None# 'OBSTYPE' type of observation i.e. BIAS, FLAT, OBJ...
+        self.radecSys = 'FK5' # prob won't change
+        self.radecEq = 2000# prob won't change
+        self.ra = None
+        self.dec = None
         
         # ---------parameters from SAM adaptive optics module (AOM)--------- #
         
         ### motor controllers, switches, and telemetry boards of AOM are connected to the instrument computer (IC)
-        self.telRA = {'TELRA':(None, 'RA of telescope (hr)')} 
-        self.telDEC = {'TELDEC': (None, 'DEC of telescope (deg)')}
-        self.hourangle = {'HA': (None, 'Hourangle (H:M:S)')} 
-        self.airmass = {'AIRMASS': (None, 'Airmass')} 
+        self.telRA = None
+        self.telDEC = None
+        self.hourangle = None
+        self.airmass = None
         
         ## position angle of the RA and DEC axes relative to the +Y axis of CCD, positive is CCW        
-        self.rapangl = {'RAPANGL': (None, 'Position angle of the RA axis (deg)')} # position angle PA-90
-        self.decpangl = {'DECPANGL': (None, 'Position angle of the DEC axis (deg)')} # position angle PA
+        self.rapangl = None # position angle PA-90
+        self.decpangl = None # position angle PA
         
         # ----------------------- #
         
@@ -63,26 +86,26 @@ class FITSHead():
         # but writing them here for now
         
         ### SAMOS Imaging CCD parameters
-        self.pixsize1 = {'PIXSIZE1': (13, 'Unbinned pixel size for axis 1 (microns)')}
-        self.pixsize2 = {'PIXSIZE2': (13, 'Unbinned pixel size for axis 2 (microns)')} 
-        self.pixscale1 = {'PIXSCALE1': (0.17, 'Unbinned pixel scale for axis 1 (arcsec/pixel)')} 
-        self.pixscale2 = {'PIXSCALE2': (0.17, 'Unbinned pixel scale for axis 2 (arcsec/pixel)')} 
+        self.pixsize1 = 13
+        self.pixsize2 = 13
+        self.pixscale1 = 0.17
+        self.pixscale2 = 0.17
         
         # matrix elements for astrometric solution
         
-        self.wcsdim = {'WCSDIM': (2, 'WCS dimensionality')}
-        self.ctype1 = {'CTYPE1': ('RA---TAN', 'Coordinate type')} # RA---TAN-SIP? for distorion
-        self.ctype2 = {'CTYPE2': ('DEC--TAN', 'Coordinate type')} # DEC--TAN-SIP?
-        self.crval1 = {'CRVAL1': (None, 'Coordinate reference value')} 
-        self.crval2 = {'CRVAL2': (None, 'Coordinate reference value')} 
-        self.crpix1 = {'CRPIX1': (None, 'Coordinate reference pixel')} 
-        self.crpix2 = {'CRPIX2': (None, 'Coordinate reference pixel')} 
-        self.cd11 = {'CD1_1': None}
-        self.cd22 = {'CD2_2': None}
-        self.cd12 = {'CD1_2': None}
-        self.cd21 = {'CD2_1': None}
-        self.cdelt1 = {'CDELT1': None} # degrees per pixel serial direction
-        self.cdelt2 = {'CDELT2': None} # degrees per pixel parallel direction
+        self.wcsdim = 2
+        self.ctype1 = 'RA---TAN' # RA---TAN-SIP? for distorion
+        self.ctype2 = 'DEC--TAN' # DEC--TAN-SIP?
+        self.crval1 = None
+        self.crval2 = None
+        self.crpix1 = None
+        self.crpix2 = None
+        self.cd11 = None
+        self.cd22 = None
+        self.cd12 = None
+        self.cd21 = None
+        self.cdelt1 = None # degrees per pixel serial direction
+        self.cdelt2 = None # degrees per pixel parallel direction
         
         
         # ----------------------- #
@@ -92,12 +115,12 @@ class FITSHead():
         # including params for spec side for completeness
         
         
-        self.filters = {'FILTERS': (None, 'Names of filter wheels in A and B')}
-        self.filter1 = {'FILTER1': (None, 'Name of filter wheel A')}
-        self.filter2 = {'FILTER2': (None, 'Name of filter wheel B')}
-        self.filtpos = {'FILTPOS': (None, 'Filter positions A and B')}
-        self.grating = {'GRATING' : (None, 'VPH grating name')}
-        self.dmdReg = {'DMDREG' : (None, 'Name of corresponding DMD .reg file')} # not a standard FITS keyword
+        self.filters = None
+        self.filter1 = None
+        self.filter2 = None
+        self.filtpos = None
+        self.grating = None
+        self.dmdReg = None # not a standard FITS keyword
         
     def create_main_params_dict(self):
         
@@ -108,43 +131,44 @@ class FITSHead():
         
         ## need to put these in a better order
         
-        self.main_dict = self.filename |\
-                    self.expTime |\
-                    self.objName |\
-                    self.obsType |\
-                    self.radecSys |\
-                    self.radecEq |\
-                    self.ra |\
-                    self.dec |\
-                    self.telRA |\
-                    self.telDEC |\
-                    self.hourangle |\
-                    self.airmass | \
-                    self.rapangl |\
-                    self.decpangl |\
-                    self.pixsize1 |\
-                    self.pixsize2 |\
-                    self.pixscale1 |\
-                    self.pixscale2 |\
-                    self.wcsdim |\
-                    self.ctype1 |\
-                    self.ctype2 |\
-                    self.crval1 |\
-                    self.crval2 |\
-                    self.crpix1 |\
-                    self.crpix2 |\
-                    self.cd11 |\
-                    self.cd22 |\
-                    self.cd12 |\
-                    self.cd21 |\
-                    self.cdelt1 |\
-                    self.cdelt2 |\
-                    self.filters |\
-                    self.filter1 |\
-                    self.filter2 |\
-                    self.filtpos |\
-                    self.grating |\
-                    self.dmdReg
+        self.main_dict = {'FILENAME' : self.filename,
+                'EXPTIME': (self.expTime, 'Exposure time (s)'),
+                'OBJECT': (self.objName,'User-defined name of object'),
+                'OBSTYPE': (self.obsType, 'Type of observation'),
+                'RADECSYS': (self.radecSys, 'Default coordinate system'),
+                'RADECEQ': (self.radecEq, 'Default equinox'),
+                'RA': (self.ra,'RA of object (hr)'),
+                'DEC': (self.dec,'DEC of object (deg)'),
+                'TELRA':(self.telRA, 'RA of telescope (hr)'),
+                'TELDEC': (self.telDEC, 'DEC of telescope (deg)'),
+                'HA': (self.hourangle, 'Hourangle (H:M:S)'),
+                'AIRMASS': (self.airmass, 'Airmass'),
+                'RAPANGL': (self.rapangl, 'Position angle of the RA axis (deg)'),
+                'DECPANGL': (self.decpangl, 'Position angle of the DEC axis (deg)'),
+                'PIXSIZE1': (self.pixsize1, 'Unbinned pixel size for axis 1 (microns)'),
+                'PIXSIZE2': (self.pixsize2, 'Unbinned pixel size for axis 2 (microns)'),
+                'PIXSCALE1': (self.pixscale1, 'Unbinned pixel scale for axis 1 (arcsec/pixel)'),
+                'PIXSCALE2': (self.pixscale2, 'Unbinned pixel scale for axis 2 (arcsec/pixel)'),
+                'WCSDIM': (self.wcsdim, 'WCS dimensionality'),
+                'CTYPE1': (self.ctype1, 'Coordinate type'),
+                'CTYPE2': (self.ctype2, 'Coordinate type'),
+                'CRVAL1': (self.crval1, 'Coordinate reference value'),
+                'CRVAL2': (self.crval2, 'Coordinate reference value'),
+                'CRPIX1': (self.crpix1, 'Coordinate reference pixel'),
+                'CRPIX2': (self.crpix2, 'Coordinate reference pixel'),
+                'CD1_1': self.cd11,
+                'CD2_2': self.cd22,
+                'CD1_2': self.cd12,
+                'CD2_1': self.cd21,
+                'CDELT1': self.cdelt1,
+                'CDELT2': self.cdelt2,
+                'FILTERS': (self.filters, 'Names of filter wheels in A and B'),
+                'FILTER1': (self.filter1, 'Name of filter wheel A'),
+                'FILTER2': (self.filter2, 'Name of filter wheel B'),
+                'FILTPOS': (self.filtpos, 'Filter positions A and B'),
+                'GRATING' : (self.grating, 'VPH grating name'),
+                'DMDREG' : (self.dmdReg, 'Name of corresponding DMD .reg file')}
+        
         
     def write_fits_header(self, input_header):
         
